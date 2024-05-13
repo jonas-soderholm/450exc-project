0d23d2132be392031901b0b5d1fceb6a3cbe537f
jonas-soderholm <jonas.soderholm89@gmail.com>
2024-05-13 11:07:25 +0200

    Update spin icon

--- src/components/EXCInformation.jsx
+++ src/components/EXCInformation.jsx
@@ -89,7 +89,7 @@ export function EXCInformation() {
               }
             }}
           >
-            {showGeneralInfo ? "hide info" : "more info"}
+            {showGeneralInfo ? "Hide info" : "More info"}
           </button>
         </div>
       </div>
